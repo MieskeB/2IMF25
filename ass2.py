b4dd7225b24e10808f6d1c3c3f3de90d8dad86b9
Michel Bijnen <info@michelbijnen.nl>
2024-09-18 17:09:31 +0200

    feat: small optimalization in ass2

--- ass2.py
+++ ass2.py
@@ -20,19 +20,18 @@ rotated = [Bool(f'rotated_{i}') for i in range(num_posters)]
 # First the inside of canvas constraint
 for i in range(num_posters):
     width, height = posters[i]
-    solver.add(If(rotated[i],
-                  And(
-                      x[i] + height <= canvas_width,
-                      y[i] + width <= canvas_height,
-                      x[i] >= 0,
-                      y[i] >= 0
-                  ),
-                  And(
-                      x[i] + width <= canvas_width,
-                      y[i] + height <= canvas_height,
-                      x[i] >= 0,
-                      y[i] >= 0
-                  )))
+    solver.add(And(If(rotated[i],
+                      And(
+                          x[i] + height <= canvas_height,
+                          y[i] + width <= canvas_width
+                      ),
+                      And(
+                          x[i] + width <= canvas_width,
+                          y[i] + height <= canvas_height
+                      ))),
+               x[i] >= 0,
+               y[i] >= 0
+               )
 
 # Second the overlapping constraint
 for i in range(num_posters):
@@ -60,9 +59,9 @@ if perp_cut_line:
     cut_perp = Int('cut_perp')
     solver.add(And(cut_perp > 0 + distance_cut_from_canvas,
                    cut_perp < If(Not(cut_over_x),
-                                     canvas_height - distance_cut_from_canvas,
-                                     canvas_width - distance_cut_from_canvas
-                                     )))
+                                 canvas_height - distance_cut_from_canvas,
+                                 canvas_width - distance_cut_from_canvas
+                                 )))
 for i in range(num_posters):
     width, height = posters[i]
     solver.add(If(cut_over_x,
@@ -115,7 +114,8 @@ if solver.check() == sat:
         rect = Rectangle((x_pos, y_pos), width, height, linewidth=1, edgecolor='black', facecolor='lightblue')
         ax.add_patch(rect)
         # Label each poster with its number
-        ax.text(x_pos + width / 2, y_pos + height / 2, str(i + 1) + "\n(" + str(width) + "x" + str(height) + ")", color='black', ha='center', va='center')
+        ax.text(x_pos + width / 2, y_pos + height / 2, str(i + 1) + "\n(" + str(width) + "x" + str(height) + ")",
+                color='black', ha='center', va='center')
 
     # Plot the cut line
     cut_pos = model[cut].as_long()
@@ -134,7 +134,6 @@ if solver.check() == sat:
         else:
             print(f"Vertical cut at x = {cut_perp_pos}")
             ax.axvline(cut_perp_pos, color='red', linestyle='--', label=f'Cut at x = {cut_perp_pos}')
-
 
     # Display the plot
     plt.title("Posters")
