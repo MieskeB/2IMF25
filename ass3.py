cb7b64124b7bad2b8b449b1f418c9ad40fd07530
Michel Bijnen <info@michelbijnen.nl>
2024-09-23 17:01:28 +0200

    feat: fixed bug and added lasso constraint

--- ass3.py
+++ ass3.py
@@ -1,6 +1,6 @@
 from z3 import *
 
-T = 150
+T = 250
 
 initial_food_a = 60
 initial_food_b = 60
@@ -9,7 +9,7 @@ food_capacity_a = 90
 food_capacity_b = 120
 food_capacity_c = 90
 
-truck_food_capacity = 273
+truck_food_capacity = 150
 
 truck_at_S = [Bool(f"truck_at_S_{t}") for t in range(T)]
 truck_at_A = [Bool(f"truck_at_A_{t}") for t in range(T)]
@@ -26,7 +26,7 @@ drop_A = [Int(f"drop_A_{t}") for t in range(T)]
 drop_B = [Int(f"drop_B_{t}") for t in range(T)]
 drop_C = [Int(f"drop_C_{t}") for t in range(T)]
 
-truck_load = [Int(f"trucK_load_{t}") for t in range(T)]
+truck_load = [Int(f"truck_load_{t}") for t in range(T)]
 
 solver = Solver()
 
@@ -36,6 +36,8 @@ solver.add(truck_load[0] == truck_food_capacity)
 solver.add(food_remaining_A[0] == initial_food_a)
 solver.add(food_remaining_B[0] == initial_food_b)
 solver.add(food_remaining_C[0] == initial_food_c)
+
+# region Truck movement
 
 # Ensure truck can only be in 1 location at the same time
 for t in range(T):
@@ -78,21 +80,26 @@ for t in range(1, T):
     solver.add(Implies(truck_at_C[t], Or(*cc, False)))
 
 # Maintain last location (needed because a truck can decide to drive to multiple locations)
-for t in range(T - 1):
+for t in range(T):
     solver.add(If(truck_at_S[t], last_location[t] == 0,
                   If(truck_at_A[t], last_location[t] == 1,
                      If(truck_at_B[t], last_location[t] == 2,
                         If(truck_at_C[t], last_location[t] == 3, last_location[t] == last_location[t - 1])))))
 
-# Truck refill and maintain
-for t in range(T - 1):
+# endregion
+
+# region Truck (un)loading
+
+# Truck boundaries
+for t in range(T):
     solver.add(truck_load[t] <= truck_food_capacity)
     solver.add(truck_load[t] >= 0)
-    solver.add(If(truck_at_S[t],
-                  truck_load[t] == truck_food_capacity,
-                  Implies(Not(Or(truck_at_A[t], truck_at_B[t], truck_at_C[t])),
-                          truck_load[t] == truck_load[t + 1]
-                          )))
+
+# Truck refill and maintain
+for t in range(1, T):
+    solver.add(Implies(truck_at_S[t], truck_load[t] == truck_food_capacity))
+    solver.add(Implies(Not(Or(truck_at_S[t], truck_at_A[t], truck_at_B[t], truck_at_C[t])),
+                       truck_load[t] == truck_load[t - 1]))
 
 # Town capacity
 for t in range(T):
@@ -101,44 +108,48 @@ for t in range(T):
     solver.add(food_remaining_C[t] <= food_capacity_c)
 
 # Truck delivery
-for t in range(1, T - 1):
+for t in range(1, T):
     solver.add(And(
         drop_A[t] >= 0,
-        drop_A[t] <= If(truck_load[t] < food_capacity_a - food_remaining_A[t],
-                        truck_load[t],
+        drop_A[t] <= If(truck_load[t - 1] < food_capacity_a - food_remaining_A[t - 1],
+                        truck_load[t - 1],
                         food_capacity_a - food_remaining_A[t]
                         )))
     solver.add(And(
         drop_B[t] >= 0,
-        drop_B[t] <= If(truck_load[t] < food_capacity_b - food_remaining_B[t],
-                        truck_load[t],
+        drop_B[t] <= If(truck_load[t - 1] < food_capacity_b - food_remaining_B[t - 1],
+                        truck_load[t - 1],
                         food_capacity_b - food_remaining_B[t]
                         )))
     solver.add(And(
         drop_C[t] >= 0,
-        drop_C[t] <= If(truck_load[t] < food_capacity_c - food_remaining_C[t],
-                        truck_load[t],
+        drop_C[t] <= If(truck_load[t - 1] < food_capacity_c - food_remaining_C[t - 1],
+                        truck_load[t - 1],
                         food_capacity_c - food_remaining_C[t]
                         )))
 
     solver.add(Implies(truck_at_A[t], And(
-        truck_load[t + 1] == truck_load[t] - drop_A[t],
-        food_remaining_A[t + 1] == food_remaining_A[t] + drop_A[t] - 1
+        truck_load[t] == truck_load[t - 1] - drop_A[t],
+        food_remaining_A[t] == food_remaining_A[t - 1] + drop_A[t] - 1
     )))
     solver.add(Implies(truck_at_B[t], And(
-        truck_load[t + 1] == truck_load[t] - drop_B[t],
-        food_remaining_B[t + 1] == food_remaining_B[t] + drop_B[t] - 1
+        truck_load[t] == truck_load[t - 1] - drop_B[t],
+        food_remaining_B[t] == food_remaining_B[t - 1] + drop_B[t] - 1
     )))
     solver.add(Implies(truck_at_C[t], And(
-        truck_load[t + 1] == truck_load[t] - drop_C[t],
-        food_remaining_C[t + 1] == food_remaining_C[t] + drop_C[t] - 1
+        truck_load[t] == truck_load[t - 1] - drop_C[t],
+        food_remaining_C[t] == food_remaining_C[t - 1] + drop_C[t] - 1
     )))
 
+# endregion
+
+# region City eating
+
 # Making sure that people eat food every time unit
-for t in range(T - 1):
-    solver.add(Implies(Not(truck_at_A[t]), food_remaining_A[t] - 1 == food_remaining_A[t + 1]))
-    solver.add(Implies(Not(truck_at_B[t]), food_remaining_B[t] - 1 == food_remaining_B[t + 1]))
-    solver.add(Implies(Not(truck_at_C[t]), food_remaining_C[t] - 1 == food_remaining_C[t + 1]))
+for t in range(1, T):
+    solver.add(Implies(Not(truck_at_A[t]), food_remaining_A[t] == food_remaining_A[t - 1] - 1))
+    solver.add(Implies(Not(truck_at_B[t]), food_remaining_B[t] == food_remaining_B[t - 1] - 1))
+    solver.add(Implies(Not(truck_at_C[t]), food_remaining_C[t] == food_remaining_C[t - 1] - 1))
 
 # Check for starvation
 for t in range(T):
@@ -146,6 +157,36 @@ for t in range(T):
     solver.add(food_remaining_B[t] >= 0)
     solver.add(food_remaining_C[t] >= 0)
 
+# endregion
+
+# region works indefinitely proof
+
+res1 = Int('t1')
+res2 = Int('t2')
+
+exists_condition = False
+for t1 in range(T - 1):
+    for t2 in range(t1, T):
+        same_state = And(
+            Or(
+                And(truck_at_S[t1], truck_at_S[t2]),
+                And(truck_at_A[t1], truck_at_A[t2]),
+                And(truck_at_B[t1], truck_at_B[t2]),
+                And(truck_at_C[t1], truck_at_C[t2]),
+            ),
+            truck_load[t1] == truck_load[t2],
+            food_remaining_A[t1] == food_remaining_A[t2],
+            food_remaining_B[t1] == food_remaining_B[t2],
+            food_remaining_C[t1] == food_remaining_C[t2],
+            last_location[t1] == last_location[t2],
+            res1 == t1,
+            res2 == t2
+        )
+        exists_condition = Or(exists_condition, same_state)
+
+solver.add(exists_condition)
+
+# endregion
 
 if solver.check() == sat:
     model = solver.model()
@@ -164,6 +205,16 @@ if solver.check() == sat:
             print(f"Amount of food at town B: {model[food_remaining_B[i]]}")
             print(f"Amount of food at town C: {model[food_remaining_C[i]]}")
 
-        # print(f"Prev loc: {model[last_location[i]]}")
+        t1 = model[res1]
+        t2 = model[res2]
+        print(f"There is a matching state at {t1} and {t2}")
+        print(
+            f"{t1}: Food: {model[food_remaining_A[t1]]}, {model[food_remaining_B[t1]]}, {model[food_remaining_C[t1]]}")
+        print(
+            f"    Truck load: {model[truck_load[t1]]}, last location: {model[last_location[t1]]}")
+        print(
+            f"{t2}: Food: {model[food_remaining_A[t2]]}, {model[food_remaining_B[t2]]}, {model[food_remaining_C[t2]]}")
+        print(
+            f"    Truck load: {model[truck_load[t2]]}, last location: {model[last_location[t2]]}")
 else:
     print("No solutions found")
