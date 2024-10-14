6d85c1aa989e7c4422f6dd595b05d9c626230540
Michel Bijnen <info@michelbijnen.nl>
2024-10-14 08:39:44 +0200

    feat: made the code functioning

--- ass5.py
+++ ass5.py
@@ -4,10 +4,9 @@ from buddy.buddy import BuDDy
 
 directory = 'ass5'
 
-var_order = list(range(1000))
-manager = BuDDy(var_order, "buddy/buddy.windows")
 
-def process_file(filename):
+def process_dimacs_file(filename):
+    print(f"Now processing {filename}")
     f = os.path.join(directory, filename)
 
     if not os.path.isfile(f):
@@ -18,32 +17,63 @@ def process_file(filename):
     lines = file.readlines()
 
     number_of_variables = 0
+    var_order = []
     code = []
     for line in lines:
         if line.startswith('p'):
-            # 'p cnf ' = 6 characters
             number_of_variables = int(line.split(' ')[2])
+        elif line.startswith('c vo'):
+            var_order = [int(x) for x in line.split()[2:]]
         elif not line.startswith('c'):
             for c in line.split(' '):
                 code.append(int(c))
 
-    x = [manager.var2bdd(i) for i in range(number_of_variables)]
+    if not var_order:
+        var_order = list(range(1, number_of_variables + 1))
 
+    return code, var_order
+
+
+def run_bdd(clauses_array, variable_count, var_order):
     clauses = manager.true
     curr = manager.false
-    for c in code:
+    print(f"Processing {len(clauses_array)} clauses")
+    for c in clauses_array:
         if c == 0:
             clauses = manager.apply_and(clauses, curr)
             curr = manager.false
         else:
             if c > 0:
-                curr = manager.apply_or(curr, x[c - 1])
+                curr = manager.apply_or(curr, manager.var2bdd(c))
             else:
-                curr = manager.apply_or(curr, manager.neg(x[abs(c) - 1]))
+                curr = manager.apply_or(curr, manager.nvar2bdd(abs(c)))
 
-    return manager.satcount(clauses)
+    node_count = manager.node_count(clauses)
+    print(f"Node count: {node_count}")
+    sat_count = manager.satcount(clauses)
+    print(f"Possible configurations: {sat_count}")
+
+    print("=-= RESULTS =-=")
+    print(f"A) {sat_count}")
+    print(f"B) apple")
+    print(f"  i) apple")
+    print(f"  ii) apple")
+    print(f"  iii) apple")
+    print(f"  iv) apple")
+    print(f"C) apple")
 
 
-for filename in os.listdir(directory):
-    result = process_file(filename)
-    print(f"File {filename} SAT count: {result}")
+if __name__ == "__main__":
+    # filename = "buildroot"
+    # filename = "busybox"
+    filename = "embtoolkit"
+    # filename = "toybox"
+    # filename = "uClinux"
+
+    filename += ".dimacs"
+
+    print(f"=-=-= {filename} =-=-=")
+    clauses_array, var_order = process_dimacs_file(filename)
+    manager = BuDDy(var_order, "buddy/buddy.windows")
+
+    print(f"=-=-= {filename} =-=-=")
